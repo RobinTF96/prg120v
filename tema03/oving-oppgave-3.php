5146b5b64ee23d79254ac99184cb9eba3b896385
RobinTF96 <rofin5165@usn.no>
2025-09-29 18:05:05 +0200

    w

--- tema03/oving-oppgave-3.php
+++ tema03/oving-oppgave-3.php
@@ -1,12 +1,16 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 $opg1=$_POST["oppgave1"] ?? null;
+$opg2=$_POST["oppgave2"] ?? null;
 
 if ($opg1=="oppgave1") {
     for ($tall1 = 1; $tall1 <= 10; $tall1++) {
         echo $tall1 . "<br />";
+    }
+}
+
+if ($opg2=="oppgave2") {
+    for ($tall2 = 1; $tall2 >= 1; $tall2++) {
+        echo ($tall2 * $tall2) . "<br />";
     }
 }
 ?>
