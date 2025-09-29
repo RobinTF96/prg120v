d0653eef2ca16c8a67421402c6d20dad7bb7a151
RobinTF96 <rofin5165@usn.no>
2025-09-29 18:31:30 +0200

    w

--- tema03/oving-oppgave-3.php
+++ tema03/oving-oppgave-3.php
@@ -3,6 +3,7 @@ $opg1=$_POST["oppgave1"] ?? null;
 $opg2=$_POST["oppgave2"] ?? null;
 $opg3=$_POST["oppgave3"] ?? null;
 $opg4=$_POST["oppgave4"] ?? null;
+$sum=0;
 
 if ($opg1=="oppgave1") {
     for ($tall1 = 1; $tall1 <= 10; $tall1++) {
