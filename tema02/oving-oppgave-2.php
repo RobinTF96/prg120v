bd2a29e5b2704bff47c264e43209e0524f28325c
RobinTF96 <rofin5165@usn.no>
2025-09-29 15:07:46 +0200

    w

--- tema02/oving-oppgave-2.php
+++ tema02/oving-oppgave-2.php
@@ -1,9 +1,16 @@
 <?php
-$svar=$_POST ["svar"];
+$svar1=$_POST ["svar1"];
+$svar2=$_POST ["svar2"];
 
 if ($svar==9) {
     print ("Riktig svar! 3 ganger 3 er 9.");
 } else {
     print ("Feil svar. 3 ganger 3 er ikke $svar.");
+}
+
+if ($svar2=="ja" or $svar2=="Ja" or $svar2=="JA" or $svar2=="j") {
+    print ("Du er student på USN.");
+} else {
+    print("Du er ikke student på USN.");
 }
 ?>
