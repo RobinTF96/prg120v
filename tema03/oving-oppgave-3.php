3ee55dcbaeec0d489ed5e8da09c6ce6b14ba0795
RobinTF96 <rofin5165@usn.no>
2025-09-29 17:42:43 +0200

    w

--- tema03/oving-oppgave-3.php
+++ tema03/oving-oppgave-3.php
@@ -1,8 +1,9 @@
+<?php
 $opg1=$_POST["oppgave1"] ?? null;
 
-if (!empty($opg1)) {
-    // Kjør oppgave 1
+if ($opg1=="oppgave1") {
     for ($tall1 = 1; $tall1 <= 10; $tall1++) {
         echo $tall1 . "<br />";
     }
 }
+?>
