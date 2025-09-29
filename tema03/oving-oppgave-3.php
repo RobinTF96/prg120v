430e91029db00fe7fc2000372a74e006a7e14e17
RobinTF96 <rofin5165@usn.no>
2025-09-29 18:18:25 +0200

    g

--- tema03/oving-oppgave-3.php
+++ tema03/oving-oppgave-3.php
@@ -1,6 +1,7 @@
 <?php
 $opg1=$_POST["oppgave1"] ?? null;
 $opg2=$_POST["oppgave2"] ?? null;
+$opg3=$_POST["oppgave3"] ?? null;
 
 if ($opg1=="oppgave1") {
     for ($tall1 = 1; $tall1 <= 10; $tall1++) {
@@ -12,5 +13,20 @@ if ($opg2=="oppgave2") {
     for ($tall2 = 1; $tall2 <= 10; $tall2++) {
         echo "Kvadratet av " . $tall2 . " er " . ($tall2 * $tall2) . ".<br />";
     }
+}
+
+if ($opg3=="oppgave3") {
+    for ($tall3 = 1; $tall3 <= 10; $tall3++) {
+        echo $tall3 . " ";
+    }
+    echo "<br />";
+    for ($tall3 = 11; $tall3 <= 20; $tall3++) {
+        echo $tall3 . " ";
+    }
+    echo "<br />";
+    for ($tall3 = 21; $tall3 <= 30; $tall3++) {
+        echo $tall3 . " ";
+    }
+    echo "<br />";
 }
 ?>
