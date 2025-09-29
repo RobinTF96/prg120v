6ece5517af546caa2e8f6c7f146cbd3a3c157984
RobinTF96 <rofin5165@usn.no>
2025-09-29 16:53:04 +0200

    w

--- tema02/oving-oppgave-2.php
+++ tema02/oving-oppgave-2.php
@@ -1,6 +1,10 @@
 <?php
 $svar1=$_POST ["svar1"] ?? null;
 $svar2=$_POST ["svar2"] ?? null;
+$svar3=$_POST ["svar3"] ?? null;¨
+$gift=$_POST ["gift"] ?? null;
+$barn=$_POST ["barn"] ?? null;
+
 
 if (!empty($svar1)) {
     if ($svar1==9) {
@@ -13,8 +17,26 @@ if (!empty($svar1)) {
 if (!empty($svar2)) {
     if (in_array(strtolower($svar2), ["ja", "j"])){
         print ("Du er student på USN.<br>");
-    } else {
+    } elseif (in_array(strtolower($svar2), ["nei", "n"])) {
         print("Du er ikke student på USN.<br>");
     }
+    else {
+        print("Ugyldig svar. Vennligst svar med 'ja' eller 'nei'.<br>");
+    }
 }
+
+if (!empty($gift) && !empty($barn)) {
+    if (($gift == "gift" || $gift == "ugift") && ($barn == "barn" || $barn == "uten_barn")) {
+        $tekst1 = "Du er " . $gift;
+        if ($barn == "barn") {
+            $tekst1 .= " og har barn";
+        } else {
+            $tekst1 .= " og har heldigvis ikke barn";
+        }
+        print($tekst1 . ".<br>");
+    } else {
+        print("Ugyldig valg. Vennligst velg gyldige alternativer.<br>");
+    }
+}
+
 ?>
