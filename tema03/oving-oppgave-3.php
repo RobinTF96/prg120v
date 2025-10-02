456207f47f805dee3e1318484fe11a4124919f97
RobinTF96 <rofin5165@usn.no>
2025-10-02 11:46:56 +0200

    w

--- tema03/oving-oppgave-3.php
+++ tema03/oving-oppgave-3.php
@@ -7,7 +7,13 @@ $opg5=$_POST["oppgave5"] ?? null;
 $opg6=intVal($_POST["oppgave6"] ?? 0);
 $opg7_fornavn=$_POST["fornavn"] ?? null;
 $opg7_etternavn=$_POST["etternavn"] ?? null;
-$opg8_array[]=intVal($_POST["t1", "t2", "t3", "t4", "t5"] ?? 0);
+$opg8_array= [
+    intVal($_POST["t1"] ?? 0);
+    intVal($_POST["t2"] ?? 0);
+    intVal($_POST["t3"] ?? 0);
+    intVal($_POST["t4"] ?? 0);
+    intVal($_POST["t5"] ?? 0);
+];
 $sum4=0;
 $sum5=0;
 $gjennomsnitt=0;
