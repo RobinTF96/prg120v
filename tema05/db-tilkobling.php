75a765f46786d1755262e090fb4c59731578a1a6
RobinTF96 <rofin5165@usn.no>
2025-10-06 16:00:39 +0200

    w

--- tema05/db-tilkobling.php
+++ tema05/db-tilkobling.php
@@ -4,6 +4,6 @@ $username = getenv('DB_USER');
 $password = getenv('DB_PASS');
 $database = getenv('DB_DATABASE');
 
-$db = mysqli_connect($host, $username, $password, $database) or die ("Kunne ikke koble til databasen"););
+$db = mysqli_connect($host, $username, $password, $database) or die ("Kunne ikke koble til databasen");
 
 ?>
