951761147cb48ae72fe469300c241c91b1f167f3
RobinTF96 <rofin5165@usn.no>
2025-10-06 20:40:12 +0200

    w

--- obl-oppgave/vis-alle-studenter.php
+++ obl-oppgave/vis-alle-studenter.php
@@ -1,7 +1,7 @@
 <?php
 include ("db-tilkobling.php");
 
-$sqlSetning = "SELECT * FROM student ORDER BY brukernavn;";
+$sqlSetning = "SELECT * FROM student ORDER BY klassekode;";
 $sqlResultat = mysqli_query($db, $sqlSetning) or die ("Ikke mulig å hente data fra databasen");
 $antallRader = mysqli_num_rows($sqlResultat);
 
